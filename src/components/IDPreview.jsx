9997d70d35b74fd64edc11bbe983bdbb4ecb6eb0
Soundar-Arunachalam <soundararunachalam3639@gmail.com>
2024-10-05 21:45:28 +0530

    deleted redundant files

--- src/components/IDPreview.jsx
+++ src/components/IDPreview.jsx
@@ -2,6 +2,7 @@ import React, { useState,useEffect } from 'react';
 import { Box, Typography, Avatar, Divider, Button,IconButton,TextField } from '@mui/material';
 import frameImage from "../assets/idframe.png"
 import axios from "axios";
+import Confetti from 'react-confetti';  
 const loadImage = (src) => {
   return new Promise((resolve, reject) => {
     const img = new Image();
@@ -10,56 +11,64 @@ const loadImage = (src) => {
     img.onerror = reject;
   });
 };
-const sendEmail=async (email,finalImage,setEmail)=>{
-  try{
-const reponse=await axios.post("http://localhost:5000/send-email", {
-        to_email: email,
-        image_data: finalImage,
-      });
-    }
-    catch(err){
-      console.log(err);
-    }
-    setEmail("");
-}
 
-const PreviewID=async(name,rollNo,regNo,Degree,Department,image,setFinalImage)=>{
-  try {
-    const [capturedImg, frameImg] = await Promise.all([
-      loadImage(image),
-      loadImage(frameImage),
-    ]);
-    const canvas = document.createElement("canvas");
-      const ctx = canvas.getContext("2d");
 
-      canvas.width = capturedImg.width;
-      canvas.height = capturedImg.height;
-
-      ctx.drawImage(frameImg, 0, 0, canvas.width, canvas.height);
-      ctx.drawImage(capturedImg, 10, 130, canvas.width - 280, canvas.height-70);
-      ctx.font = "20px sans serif";
-      ctx.fillStyle = "blue";
-      ctx.fillText(name,190,200);
-      console.log(name);
-      ctx.font = "20px Arial";
-      ctx.fillStyle="red"
-      ctx.fillText(rollNo+" / "+regNo,200,240);
-      ctx.fillStyle="green"
-      ctx.fillText(Degree+"/"+Department,200,280);
-      
-      
-      
-
-      const finalImage = canvas.toDataURL("image/png");
-
-      setFinalImage(finalImage);
-  }
-  catch(err){
-console.log("Operation Failed");
-  }
-}
 
 const IDPreview = ({ data,img }) => {
+  const sendEmail=async (email,finalImage,setEmail)=>{
+    try{
+  const reponse=await axios.post("http://localhost:5000/send-email", {
+          to_email: email,
+          image_data: finalImage,
+        });
+      }
+      catch(err){
+        console.log(err);
+      }
+      setEmail("");
+      setShowConfetti(true);
+      setTimeout(() => {
+        setShowConfetti(false);
+        
+      }, 5000);
+  }
+  
+  const [showConfetti, setShowConfetti] = useState(false); 
+  const PreviewID=async(name,rollNo,regNo,Degree,Department,image,setFinalImage)=>{
+    try {
+      const [capturedImg, frameImg] = await Promise.all([
+        loadImage(image),
+        loadImage(frameImage),
+      ]);
+      const canvas = document.createElement("canvas");
+        const ctx = canvas.getContext("2d");
+  
+        canvas.width = capturedImg.width;
+        canvas.height = capturedImg.height;
+  
+        ctx.drawImage(frameImg, 0, 0, canvas.width, canvas.height);
+        ctx.drawImage(capturedImg, 10, 130, canvas.width - 280, canvas.height-70);
+        ctx.font = "20px sans serif";
+        ctx.fillStyle = "blue";
+        ctx.fillText(name,190,200);
+        console.log(name);
+        ctx.font = "20px Arial";
+        ctx.fillStyle="red"
+        ctx.fillText(rollNo+" / "+regNo,200,240);
+        ctx.fillStyle="green"
+        ctx.fillText(Degree+"/"+Department,200,280);
+        
+        
+        
+  
+        const finalImage = canvas.toDataURL("image/png");
+  
+        setFinalImage(finalImage);
+    }
+    catch(err){
+  console.log("Operation Failed");
+    }
+  }
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
   };
@@ -76,7 +85,7 @@ const [finalImage, setFinalImage] = useState(null);
   return (
     
     <>
-     
+     {showConfetti && <Confetti />}
      <Box>
  <TextField
             
